a301219d0a17c768691dd3451e428b0fbbdff622
Gene De Lisa <gene@rockhoppertech.com>
2014-07-13 11:34:48 -0400

    initial

--- NonAnonymousClass/ViewController.swift
+++ NonAnonymousClass/ViewController.swift
@@ -8,11 +8,72 @@
 
 import UIKit
 
+
+protocol SimpleProtocol {
+    func didReceiveResults(s: [String]) -> Void
+}
+
 class ViewController: UIViewController {
-                            
+    
+    var resp:NonAnon = NonAnon()
+    
+    var someString = "Some String"
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.frob("nonanon", delegate:resp)
+
+        // cannot do these closures
+        
+//        var handler:SimpleProtocol = { (s:[String]) -> Void in
+//            for str in s {
+//                println(str)
+//            }
+//        }
+        
+//        frob("hi") { (a:[String]) in
+//            
+//        }
+//        
+//        frob("hi")  {
+//            
+//        }
+
+        
+        var handler:SimpleProtocol = resp
+        self.frob("handler", delegate:handler)
+        // self.frob("handler", delegate:resp)
+
+        
+        handler = NonAnonDelegate(c: self)
+        self.frob("delegate", delegate:handler)
+        
+        class NestedNonAnonDelegate:SimpleProtocol {
+            var vc:ViewController?
+            
+            init(c:ViewController) {
+                self.vc = c
+            }
+            
+            func didReceiveResults(s: [String]) -> Void {
+                println(s)
+                for str in s {
+                    println(str)
+                }
+                vc?.blob()
+                // blows up XCode
+                //blob()
+               // println(someString)
+            }
+            
+        }
+        
+        handler = NestedNonAnonDelegate(c: self)
+        self.frob("nested delegate", delegate:handler)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +81,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func frob(s:String, delegate: SimpleProtocol) {
+        println(s)
+        delegate.didReceiveResults(["foo", "bar"])
+    }
+    
+    func blob() {
+        println("blob")
+    }
 }
 
+
+class NonAnonDelegate:SimpleProtocol {
+    var vc:ViewController?
+
+    init(c:ViewController) {
+        self.vc = c
+    }
+    
+    func didReceiveResults(s: [String]) -> Void {
+        println(s)
+        for str in s {
+            println(str)
+        }
+        vc?.blob()
+    }
+    
+}
+
+
+class NonAnon:SimpleProtocol {
+    func didReceiveResults(s: [String]) -> Void {
+        println(s)
+        for str in s {
+            println(str)
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# NonAnonymousClass
+
+A simple example showing Swift alternatives for Hava anonymous inner classes
+
+## Blog post for this example.
+
+[Blog post](http://www.rockhoppertech.com/blog/swift-workaround-for-closure-as-protocol/)
+
+## Licensing
+
+This project is in the public domain. Not much to it.
+
+## Credits
+
+*	[Gene De Lisa](http://rockhoppertech.com/blog/)
